dea9be8563d3c14470d20635ed7efb9b919ea325
Diego Brito <diegorbrito9@gmail.com>
2022-08-20 18:46:43 -0300

    new: adicionando middleware para permitir receber json

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,6 @@ import 'dotenv/config';
 const app = express();
 const { PORT } = process.env;
 
+app.use(express.json())
+
 app.listen(PORT, () => console.log('Server Up\nhttp://localhost:%s', PORT));
