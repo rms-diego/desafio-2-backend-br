74e5ce62a366c12e9adf7dfc4f05941c3fc3ec56
Diego Brito <diegorbrito9@gmail.com>
2022-08-20 20:36:58 -0300

    fix: consertando erro do middleware de validação do token

--- src/middleware/validateTokenMiddleware.ts
+++ src/middleware/validateTokenMiddleware.ts
@@ -1,16 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import token from "../service/token";
 
-export default (request: Request, response: Response, next: NextFunction) => {
+export default (request: Request, _response: Response, next: NextFunction) => {
   const { token: authorization } = request.headers;
+  
 
   if (!authorization) throw new Error("Require token on header");
   
-
   const tokenDecoded = token.decode(authorization as string);
 
-  if (tokenDecoded && typeof tokenDecoded  === 'string') {
-    request.headers.authorization = tokenDecoded;
-    next()
-  }
+  request.headers.authorization = JSON.stringify(tokenDecoded);
+  next()
+  
 }
